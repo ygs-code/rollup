08e1dde6c8be04cea93094fe543d8dd577e756be
姚观寿 <yaoguanshou@rainbowcn.com>
2023-03-03 09:54:45 +0800

    gollup配置

--- bin/rollup.config.js
+++ bin/rollup.config.js
@@ -3,16 +3,74 @@ import babel from "rollup-plugin-babel"; // rollup 的 babel 插件，ES6转ES5
 import replace from "rollup-plugin-replace"; // 替换待打包文件里的一些变量，如 process在浏览器端是不存在的，需要被替换
 import commonjs from "rollup-plugin-commonjs"; // 将非ES6语法的包转为ES6可用
 import { uglify } from "rollup-plugin-uglify"; // 压缩包
-import postcss from 'rollup-plugin-postcss'
+import postcss from "rollup-plugin-postcss";
 import path from "path";
 const env = process.env.NODE_ENV;
 const config = {
   input: path.join(process.cwd(), "/src/index.js"),
   external: ["react", "redux"], // 告诉rollup，不打包react,redux;将其视为外部依赖
   output: [
-    { file: "dist/index.umd.js", format: "umd", name: "fre", sourcemap: true },
-    { file: "dist/index.js", format: "esm", sourcemap: true },
-    { file: "dist/index.esm.js", format: "esm", sourcemap: true },
+    {
+      file: "dist/es/index.js",
+      format: "es",
+      sourcemap: true,
+      globals: {
+        react: "React", // 这跟external 是配套使用的，指明global.React即是外部依赖react
+        redux: "Redux",
+      },
+    },
+    {
+      file: "dist/umd/index.js",
+      format: "umd",
+      name: "index",
+      sourcemap: true,
+      globals: {
+        react: "React", // 这跟external 是配套使用的，指明global.React即是外部依赖react
+        redux: "Redux",
+      },
+    },
+    {
+      file: "dist/amd/index.js",
+      format: "amd",
+      name: "index",
+      sourcemap: true,
+      globals: {
+        react: "React", // 这跟external 是配套使用的，指明global.React即是外部依赖react
+        redux: "Redux",
+      },
+    },
+    {
+      file: "dist/cjs/index.js",
+      name: "index",
+      format: "cjs",
+      sourcemap: true,
+      globals: {
+        react: "React", // 这跟external 是配套使用的，指明global.React即是外部依赖react
+        redux: "Redux",
+      },
+    },
+
+    {
+      file: "dist/iife/index.js",
+      name: "index",
+      format: "iife",
+      sourcemap: true,
+      globals: {
+        react: "React", // 这跟external 是配套使用的，指明global.React即是外部依赖react
+        redux: "Redux",
+      },
+    },
+
+    {
+      file: "dist/system/index.js",
+      name: "index",
+      format: "system",
+      sourcemap: true,
+      globals: {
+        react: "React", // 这跟external 是配套使用的，指明global.React即是外部依赖react
+        redux: "Redux",
+      },
+    },
   ],
   plugins: [
     nodeResolve(),
@@ -24,7 +82,7 @@ const config = {
       "process.env.NODE_ENV": JSON.stringify(env),
     }),
     commonjs(),
-    postcss(),  // css
+    postcss(), // css
     // less(),  // css
     // scss(),
   ],
@@ -42,5 +100,4 @@ if (env === "production") {
     })
   );
 }
- export default config;
- 
+export default config;
